89a9fce75651e200be3f12711c4a9d8159882f2f
Johan Félisaz <johan.felisaz@protonmail.com>
2020-09-01 15:23:09 +0200

    simple webgl

--- reactapp/graph.js
+++ reactapp/graph.js
@@ -2,11 +2,63 @@ import * as React from 'react';
 import { View, Text, Button, Switch, TextInput, FlatList, SectionList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { GLView } from 'expo-gl';
+
 
 import { styles } from './styles.js'
 import { SearchHeaderComponent, SearchScreen } from './search.js'
 
 const Stack = createStackNavigator();
+
+function GraphView() {
+    function onContextCreate(gl) {
+	const vertSrc = `
+void main(void) {
+  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+  gl_PointSize = 100.0;
+}
+`;
+	const fragSrc = `
+void main(void) {
+  gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+}
+`;
+
+	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+	gl.clearColor(0, 0, 1, 1);
+	
+	// Compile vertex and fragment shader
+	const vert = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vert, vertSrc);
+	gl.compileShader(vert);
+	const frag = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(frag, fragSrc);
+	gl.compileShader(frag);
+
+	// Link together into a program
+	const program = gl.createProgram();
+	gl.attachShader(program, vert);
+	gl.attachShader(program, frag);
+	gl.linkProgram(program);
+	gl.useProgram(program);
+
+
+	function render(now) {
+	    gl.clear(gl.COLOR_BUFFER_BIT);
+	    gl.drawArrays(gl.POINTS, 0, 1);
+	    gl.flush();
+	gl.endFrameEXP();
+	}
+
+	requestAnimationFrame(render);
+    }
+    
+    return (<View>
+	    <Text>Je suis le graphe</Text>
+	    <GLView style={{width: 300, height: 300}}
+	    onContextCreate={onContextCreate} />
+	    </View>);
+}
 
 function MainGraphScreen({ navigation }) {
     React.useEffect(() => {
@@ -14,13 +66,13 @@ function MainGraphScreen({ navigation }) {
 	    headerRight: () => (
 		    <SearchHeaderComponent
 		onFocus={() => navigation.navigate('Search')}/>
-		),
+	    ),
 	});
     });
 
     return (
 	    <View style={styles.mainView}>
-	    <Text>Je suis le graphe</Text>
+	    <GraphView />
 	    </View>
     );
 }
@@ -30,8 +82,8 @@ export function GraphScreen() {
 	    <Stack.Navigator>
 	    <Stack.Screen name="Main" component={MainGraphScreen}
 	options={({route, navigation}) => ({headerTitle: "",
-					   route: {route},
-					   navigation: {navigation}})} />
+					    route: {route},
+					    navigation: {navigation}})} />
 	    <Stack.Screen name="Search" component={SearchScreen} options={{headerTitle: ""}}/>
 	    </Stack.Navigator>
     );
