507acd8816cf2821fa2385a98f30294f32741d1a
Johan Félisaz <johan.felisaz@protonmail.com>
2020-09-01 11:18:56 +0200

    added CORS allow origin for expo web emulator

--- webserver/braincrawler/settings.py
+++ webserver/braincrawler/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bcserver.apps.BcserverConfig',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,3 +141,8 @@ REST_FRAMEWORK = {
 
 # Files used to store nodes
 NODES_PATH = env('NODES_PATH')
+
+# CORS Header 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:19006' # Expo web emulator
+]
