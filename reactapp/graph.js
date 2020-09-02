b573ed0523cbff12298c7221fe9f053de51e6846
Johan Félisaz <johan.felisaz@protonmail.com>
2020-09-02 07:39:44 +0200

    working web view

--- reactapp/graph.js
+++ reactapp/graph.js
@@ -2,8 +2,7 @@ import * as React from 'react';
 import { View, Text, Button, Switch, TextInput, FlatList, SectionList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { GLView } from 'expo-gl';
-
+import { WebView } from 'react-native-webview';
 
 import { styles } from './styles.js'
 import { SearchHeaderComponent, SearchScreen } from './search.js'
@@ -12,76 +11,9 @@ import { ViewerScreen } from './viewer.js'
 const Stack = createStackNavigator();
 
 function GraphView(props) {
-    function onContextCreate(gl) {
-	const vertSrc = `
-attribute vec2 position;
-varying vec2 uv;
-void main() {
-  gl_Position = vec4(position.x, -position.y, 0.0, 1.0);
-  uv = vec2(0.5, 0.5) * (position+vec2(1.0, 1.0));
-}`;
-	const fragSrc = `
-precision highp float;
-varying vec2 uv;
-void main () {
-  gl_FragColor = vec4(uv.x, uv.y, 0.5, 1.0);
-}`;
-
-	// Compile vertex and fragment shader
-	const vert = gl.createShader(gl.VERTEX_SHADER);
-	gl.shaderSource(vert, vertSrc);
-	gl.compileShader(vert);
-	const frag = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(frag, fragSrc);
-	gl.compileShader(frag);
-
-	// Link together into a program
-	const program = gl.createProgram();
-	gl.attachShader(program, vert);
-	gl.attachShader(program, frag);
-	gl.linkProgram(program);
-
-	// Save position attribute
-	const positionAttrib = gl.getAttribLocation(program, 'position');
-
-	// Create buffer
-	const buffer = gl.createBuffer();
-	
-	function animate(now) {
-	    try {
-		// Clear
-		gl.clearColor(0, 0, 1, 1);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		
-		// Bind buffer, program and position attribute for use
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-		gl.useProgram(program);
-		gl.enableVertexAttribArray(positionAttrib);
-		gl.vertexAttribPointer(positionAttrib, 2, gl.FLOAT, false, 0, 0);
-
-		// Buffer data and draw!
-		const speed = props.speed || 0.5;
-		const a = 0.48 * Math.sin(0.001 * speed * now) + 0.5;
-		const verts = new Float32Array([-a, -a, a, -a, -a, a, -a, a, a, -a, a, a]);
-		gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
-		gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
-
-		// Submit frame
-		gl.flush();
-		gl.endFrameEXP();
-	    } finally {
-		gl.enableLogging = false;
-		requestAnimationFrame(animate);
-	    }
-	};
-	animate();
-    }
-    
-    return (<View>
-	    <Text>Je suis le graphe</Text>
-	    <GLView style={{width: 300, height: 300}}
-	    onContextCreate={onContextCreate} />
-	    </View>);
+    return (<WebView
+            source={{ html: '<h1>Je suis un titre en HTML</h1>' }}
+	    />);
 }
 
 function MainGraphScreen({ navigation }) {
